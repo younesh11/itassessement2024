acde69ebd4f05f1ee903d0d825ea63a5de1de6fc
younesh11 <younesh.kc@gmail.com>
2025-04-11 02:30:18 +0545

    feat: add manual image slider and its resources

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.style.display = i === index ? 'block' : 'none';
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
